61b2756964b501f5ab04e22e179e29308fb5e537
SimeonNedyalkov <simeon_sg@abg.bg>
2024-07-02 15:56:12 +0300

    navbar

---
+++ vite-project/src/components/Home.jsx
@@ -0,0 +1,11 @@
+export default function Home(){
+    return(
+        <>
+        
+        <h1 className="title">Choose your Fighter!</h1>
+        <h4>Pick your fighter, choose your weapon, and prepare for battle!</h4>
+        
+        
+        </>
+    )
+}
