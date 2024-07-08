9f86eb3dd1338fc430552150f56b52a4c554ef59
SimeonNedyalkov <simeon_sg@abg.bg>
2024-07-08 13:42:44 +0300

    home and nav changed

--- vite-project/src/components/Home.jsx
+++ vite-project/src/components/Home.jsx
@@ -1,11 +1,17 @@
+import { useState } from "react"
 export default function Home(){
+    const [count,setCount] = useState(0)
+    function clickHandler(){
+        setCount(oldState => count + 1)
+        console.log(count)
+    }
     return(
         <>
-        
+        <div tabIndex={0} onKeyDown={clickHandler}>
         <h1 className="title">Choose your Fighter!</h1>
         <h4>Pick your fighter, choose your weapon, and prepare for battle!</h4>
-        
-        
+        <div>PRESS ANY BUTTON</div>
+        </div>
         </>
     )
 }
