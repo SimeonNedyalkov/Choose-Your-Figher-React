eb812215a56939a62b542020595b7fcc15a34e84
SimeonNedyalkov <simeon_sg@abv.bg>
2024-07-10 14:13:26 +0300

    About page

--- vite-project/src/components/About.jsx
+++ vite-project/src/components/About.jsx
@@ -1,9 +1,28 @@
 export default function About(){
     return (
-        <>
-        <h1>About</h1>
-        <h2>This is my About Page</h2>
-        <h3>And I will write here</h3>
-        </>
+        <main>
+        <section id="introduction">
+            <h2>Introduction</h2>
+            <p>Welcome to Choose Your Fighter, where you can select your champion and engage in epic battles!</p>
+        </section>
+
+        <section id="how-to-play">
+            <h2>How to Play</h2>
+            <p>1. Choose your fighter from a variety of exciting options, including bears, octopuses, tigers, sharks, and more!</p>
+            <p>2. Select a weapon to equip your fighter with.</p>
+            <p>3. The app will calculate the stats for your fighter.</p>
+            <p>4. Get ready to face off against another randomly selected fighter!</p>
+        </section>
+
+        <section id="development">
+            <h2>Development</h2>
+            <p>Choose Your Fighter is developed using React, ensuring a responsive and interactive user experience.</p>
+        </section>
+
+        <section id="credits">
+            <h2>Credits</h2>
+            <p>Icons made by Freepik from <a href="https://www.flaticon.com/" target="_blank" rel="noopener noreferrer">www.flaticon.com</a></p>
+        </section>
+    </main>
     )
 }
--- vite-project/src/components/Main.jsx
+++ vite-project/src/components/Main.jsx
@@ -1,6 +1,7 @@
 import Home from "./Home"
 import Register from "./Register"
 import Login from "./Login"
+import About from "./About"
 
 export default function Main({
     isRegisteredCloseHandler,
