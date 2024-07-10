02ce2f762059dad4aee432535327140b8196ba14
SimeonNedyalkov <simeon_sg@abv.bg>
2024-07-10 14:08:32 +0300

    about

---
+++ vite-project/src/components/About.jsx
@@ -0,0 +1,9 @@
+export default function About(){
+    return (
+        <>
+        <h1>About</h1>
+        <h2>This is my About Page</h2>
+        <h3>And I will write here</h3>
+        </>
+    )
+}
