eb62463bb1e517f89ca5e0765018ba0b7cfb8ce7
SimeonNedyalkov <simeon_sg@abv.bg>
2024-07-12 23:38:02 +0300

    footer fixed

--- vite-project/src/components/Champions.jsx
+++ vite-project/src/components/Champions.jsx
@@ -1,11 +1,11 @@
-// import React from 'react';
+import React from 'react';
 
-// export default function Fighter({ fighter, onSelect }){
-//     return (
-//         <div className="fighter-card" onClick={() => onSelect(fighter)}>
-//             <img src={fighter.image} alt={fighter.name} className="fighter-image" />
-//             <h3>{fighter.name}</h3>
-//             <p>{fighter.description}</p>
-//         </div>
-//     );
-// }
+export default function Champions({ fighter, onSelect }){
+    return (
+        <div className="fighter-card" onClick={() => onSelect(fighter)}>
+            <img src={fighter.image} alt={fighter.name} className="fighter-image" />
+            <h3>{fighter.name}</h3>
+            <p>{fighter.description}</p>
+        </div>
+    );
+}
--- vite-project/src/components/Footer.jsx
+++ vite-project/src/components/Footer.jsx
@@ -1,10 +1,21 @@
-export default function Footer(){
-    return (
-        <div className="footer-container">
-        <footer className="footer">
-            <p>© 2024 Choose Your Fighter. All rights reserved.</p>
-            <p>Developed by Simeon Nedylkov</p>
-        </footer>
+import { XMarkIcon } from '@heroicons/react/20/solid'
+
+export default function Footer() {
+  return (
+    // <div className="footer-container relative isolate gap-x-6 overflow-hidden bg-gray-800 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
+            <div>
+            <footer className="footer footer-container bg-gray-900 bg-gradient-to-r from-gray-700 to-gray-900">
+                <p className='text-white'>© 2024 Choose Your Fighter.<br/> All rights reserved.</p>
+                <div className='column'>
+                {/* <p className='text-white'>Developed by Simeon Nedylkov</p> */}
+                {/* <a
+            href="#"
+            className="flex-none rounded-full bg-gray-900 px-3.5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900"
+            >
+            Register now <span aria-hidden="true">&rarr;</span>
+            </a> */}
+            </div>
+            </footer>
     </div>
-    )
+  )
 }
