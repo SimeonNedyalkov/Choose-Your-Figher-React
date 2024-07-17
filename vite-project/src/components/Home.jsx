80adf14ea626a441c12ad97a13e829cff5c15801
SimeonNedyalkov <simeon_sg@abv.bg>
2024-07-17 17:35:30 +0300

    animation comment added

--- vite-project/src/components/Home.jsx
+++ vite-project/src/components/Home.jsx
@@ -1,12 +1,15 @@
-export default function Home(){
-    
-    return(
-        <>
-        <div className="homePage" tabIndex={0}>
+export default function Home() {
+  return (
+    <div className="homePage" tabIndex={0}>
+      <div className='contents'>
         <h1 className="title text-5xl font-bold">Choose your Fighter!</h1>
-        <h4 className="picktext text-2xl font-extralight font-bold">Pick your fighter, choose your weapon, and prepare for battle!</h4>
-        <div className="pressanybutton text-2xl font-bold">PRESS ANY BUTTON</div>
+        <h4 className="picktext text-2xl font-extralight font-bold">
+          Pick your fighter, choose your weapon, and prepare for battle!
+        </h4>
+        <div className="buttonContainer">
+          <button onClick={() => console.log('Game started')} className="startButton">Start Game</button>
         </div>
-        </>
-    )
+      </div>
+    </div>
+  );
 }
