d1a54d3cce43ddbbcc0eac4b18ee8f3910eb167b
SimeonNedyalkov <112097158+SimeonNedyalkov@users.noreply.github.com>
2024-07-19 16:50:54 +0300

    index.css changes

--- vite-project/src/components/Home.jsx
+++ vite-project/src/components/Home.jsx
@@ -1,15 +1,16 @@
 export default function Home() {
-  return (
-    <div className="homePage" tabIndex={0}>
-      <div className='contents'>
-        <h1 className="title text-5xl font-bold">Choose your Fighter!</h1>
-        <h4 className="picktext text-2xl font-extralight font-bold">
-          Pick your fighter, choose your weapon, and prepare for battle!
-        </h4>
-        <div className="buttonContainer">
-          <button onClick={() => console.log('Game started')} className="startButton">Start Game</button>
+    return (
+        <div className={`homePage animation`} tabIndex={0}>
+            <div className={`contents`}>
+                <h1 className="title text-5xl font-bold">Choose your Fighter!</h1>
+                <h4 className="picktext text-2xl font-extralight font-bold">
+                    Pick your fighter, choose your weapon, and prepare for battle!
+                </h4>
+                <div className="buttonContainer">
+                    <button onClick={() => console.log('Game started')} className="startButton">Start Game</button>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
+
 }
