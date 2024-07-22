6518a245555e0905824651521a97b5d2793bef8b
SimeonNedyalkov <112097158+SimeonNedyalkov@users.noreply.github.com>
2024-07-22 18:15:13 +0300

    homepage fixed

--- vite-project/src/components/Home.jsx
+++ vite-project/src/components/Home.jsx
@@ -15,8 +15,8 @@ export default function Home() {
             <div className={`contents`} >
                 <animated.h1 style={fade} className="title text-8xl font-bold font-mono">Choose your Fighter</animated.h1>
                 <div className="fancy-line"></div>
-                <h4 className="picktext text-2xl font-extralight font-bold">
-                "To the well-organized mind, death is but the next great adventure." - J.K. Rowling
+                <h4 className="picktext text-2xl font-extralight font-bold text-white-300 italic mt-4 mb-6">
+                "Every battle is won before it's ever fought." – Sun Tzu
                 </h4>
                 <div className="buttonContainer">
                     <button onClick={() => console.log('Game started')} className="startButton">Start Game</button>
