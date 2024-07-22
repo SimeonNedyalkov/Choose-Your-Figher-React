af7c4101bbfda87af0be8a7630ac2771063686bd
SimeonNedyalkov <112097158+SimeonNedyalkov@users.noreply.github.com>
2024-07-22 18:09:16 +0300

    react-spring added

--- vite-project/src/components/Home.jsx
+++ vite-project/src/components/Home.jsx
@@ -1,16 +1,29 @@
+import {useSpring,animated} from 'react-spring'
 export default function Home() {
+    const fade = useSpring({
+        from:{
+            opacity:0
+        },
+        to:{
+            opacity:1
+        }
+    })
     return (
-        <div className={`homePage animation`} tabIndex={0}>
-            <div className={`contents`}>
-                <h1 className="title text-5xl font-bold">Choose your Fighter!</h1>
+        
+        <div className={`homePage`}  tabIndex={0}>
+            
+            <div className={`contents`} >
+                <animated.h1 style={fade} className="title text-8xl font-bold font-mono">Choose your Fighter</animated.h1>
+                <div className="fancy-line"></div>
                 <h4 className="picktext text-2xl font-extralight font-bold">
-                    Pick your fighter, choose your weapon, and prepare for battle!
+                "To the well-organized mind, death is but the next great adventure." - J.K. Rowling
                 </h4>
                 <div className="buttonContainer">
                     <button onClick={() => console.log('Game started')} className="startButton">Start Game</button>
                 </div>
             </div>
-        </div>
-    );
 
+        </div>
+        
+    );
 }
