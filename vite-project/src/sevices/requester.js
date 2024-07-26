616048106cf53068b5c4f9074eb1c7167e8ad807
SimeonNedyalkov <112097158+SimeonNedyalkov@users.noreply.github.com>
2024-07-26 15:47:16 +0300

    requester-api created

---
+++ vite-project/src/sevices/requester.js
@@ -0,0 +1,40 @@
+async function requester(method,url,data){
+    const options = {}
+
+    const accessToken = localStorage.getItem('accessToken')
+    if(accessToken){
+        options.headers = {
+            ...options.headers,
+            "X-Authorization":accessToken
+        }   
+    }
+    if(method !== 'GET'){
+        options.method = method
+    }
+    if(data){
+        options.headers = {
+            ...options.headers,
+            'Content-type' : 'aplication/json'
+        };
+        options.body = JSON.stringify(data)
+    }
+    
+    const response = await fetch(url,options)
+    const result = await response.json()
+    if(!response.ok){
+        throw response
+    }
+    return result
+}
+export const get = requester.bind(null,'GET')
+export const post = requester.bind(null,'POST')
+export const put = requester.bind(null,'PUT')
+export const del = requester.bind(null,'DELETE')
+
+const requesterAPI = {
+    get,
+    post,
+    put,
+    del
+}
+export default requesterAPI
