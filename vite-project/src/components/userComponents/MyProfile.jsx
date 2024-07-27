ebfca6e81e7da1bd97d7750a81fcbb363726cae2
SimeonNedyalkov <112097158+SimeonNedyalkov@users.noreply.github.com>
2024-07-27 19:14:40 +0300

    MyProfile fetches the profile created fighters

--- vite-project/src/components/userComponents/MyProfile.jsx
+++ vite-project/src/components/userComponents/MyProfile.jsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import userAPI from "../../sevices/usersAPI";
+import useFetch from '../../hooks/useFetch';
 
 export default function MyProfile() {
     const [username, setUsername] = useState('');
+    const [userId,setUserId] =useState('')
 
     useEffect(() => {
         async function fetchUserInfo() {
             try {
-                const { username } = await userAPI.getUserInfo();
+                const { username,_id } = await userAPI.getUserInfo();
+                setUserId(_id)
                 setUsername(username);
             } catch (error) {
                 console.error('Error fetching user info:', error);
@@ -17,9 +20,29 @@ export default function MyProfile() {
         fetchUserInfo();
     }, []);
 
+    const fighters = useFetch('http://localhost:3030/data/fighters',[])
+    let ownedFighters = []
+    fighters.map((fighter)=>{
+        if(fighter._ownerId == userId){
+            ownedFighters.push(fighter)
+        }
+    })
+
     return (
         <div>
-            Hello, {username}
+            <div>Hello, {username}</div>
+            {ownedFighters.length>0 && (
+                <div className='backgroundImage'>
+                <div>Those are your fighters:</div>
+                <div>{ownedFighters.map(fighter=>(
+                        <div key={fighter._id}>
+                            <div>{fighter.name}</div>
+                            <div><img src={fighter.img} alt={fighter.name} /></div>
+                        </div>
+                        
+                ))}</div>
+                </div>
+            )}
         </div>
     );
 }
