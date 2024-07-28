796ac12acc48e1e21f9a0e9f8f8f0f7b5e5e0346
SimeonNedyalkov <112097158+SimeonNedyalkov@users.noreply.github.com>
2024-07-28 23:40:48 +0300

    fighter cards created at arena to pick fighter

--- vite-project/src/components/Arena/Arena.jsx
+++ vite-project/src/components/Arena/Arena.jsx
@@ -1,8 +1,19 @@
+import useFetch from "../../hooks/useFetch";
+
 export default function Arena() {
+    const fighters = useFetch('http://localhost:3030/data/fighters',[])
     return (
-        <div>
-            <div className="arenaBackground">
-                {/* Content can go here */}
+        <div className="arenaBackground">
+            <div className="chooseFighter">
+                Choose your fighter:
+            </div>
+            <div className="fightersContainer">
+                {fighters.map((fighter) => (
+                    <div key={fighter._id} className="fighterCard">
+                        <img src={fighter.img} alt={fighter.name} className="fighterImage"/>
+                        <div className="fighterName">{fighter.name}</div>
+                    </div>
+                ))}
             </div>
         </div>
     );
